01b368ac63a07e99bfb57407f6e2d9fdcc8aacdc
charliegabriella <charlotkroon@gmail.com>
2020-04-01 14:54:38 +0200

    Add Search Engine Form

--- src/components/Books/BookSearch.js
+++ src/components/Books/BookSearch.js
@@ -36,6 +36,22 @@ export default class BookSearch extends React.Component {
       console.log("AFTER FETCHINGG?!?", this.state.books.length);
       return (
         <div>
+          <h1>Books!</h1>
+          <div>
+            <form onSubmit={this.handleSearch}>
+              <label>
+                <input
+                  type="text"
+                  name="search"
+                  placeholder="Search for your book here.."
+                  onChange={this.handleChange}
+                  value={this.state.search}
+                />
+              </label>
+              <input type="submit" value="Search" />
+            </form>
+          </div>
+
           {this.state.books.length > 0 && (
             <div>
               {this.state.books.map(book => {
