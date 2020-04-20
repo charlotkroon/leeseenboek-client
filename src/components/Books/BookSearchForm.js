74f0f935c6eeaf944facebb43ca37f7436661798
charliegabriella <charlotkroon@gmail.com>
2020-04-20 17:32:30 +0200

    Delete comments

--- src/components/Books/BookSearchForm.js
+++ src/components/Books/BookSearchForm.js
@@ -1,30 +1,25 @@
 import React, { useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { login } from "../../store/login/action";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-//step 1 import useState from react so i can use it
 
 const style = {
-  Paper: { padding: 50, marginTop: 25, marginBottom: 25 }
+  Paper: { padding: 50, marginTop: 25, marginBottom: 25 },
 };
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
       margin: theme.spacing(1),
-      width: "200ch"
-    }
-  }
+      width: "200ch",
+    },
+  },
 }));
 
 export default function BookSearchForm(props) {
-  // const dispatch = useDispatch();
-
   const [keyword, set_keyword] = useState(""); //my first hook!
 
   const classes = useStyles();
@@ -37,7 +32,7 @@ export default function BookSearchForm(props) {
         </Typography>
         <form
           className={classes.root}
-          onSubmit={e => props.handleSearch(keyword)}
+          onSubmit={(e) => props.handleSearch(keyword)}
         >
           <Grid item className={classes.root}>
             <TextField
@@ -47,7 +42,7 @@ export default function BookSearchForm(props) {
               type="text"
               name="search"
               placeholder="Search..."
-              onChange={e => set_keyword(e.target.value)}
+              onChange={(e) => set_keyword(e.target.value)}
               value={keyword}
             />
           </Grid>
