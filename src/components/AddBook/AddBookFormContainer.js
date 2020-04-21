6a3970d1ae051a0a122252f69253b3ce8b0cfb1d
charliegabriella <charlotkroon@gmail.com>
2020-04-21 16:42:49 +0200

    Add Export default and BookSearchForm

--- src/components/AddBook/AddBookFormContainer.js
+++ src/components/AddBook/AddBookFormContainer.js
@@ -5,6 +5,13 @@ import { userLogin } from "../../store/login/action";
 
 class AddBookFormContainer extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Add a Book To Your Shelf</h1>
+        <BookSearchForm />
+      </div>
+    );
   }
 }
+
+export default connect(null, { userLogin })(AddBookFormContainer);
