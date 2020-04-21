1852ad2106bb598afc03c2cf8589f4c6774a34a9
charliegabriella <charlotkroon@gmail.com>
2020-04-21 16:40:25 +0200

    Add BookFormContainer

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -1,25 +1,25 @@
-import React from "react";
+// import React from "react";
 
-class Book extends React.Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <div>
-            <select>
-              <option value="move" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+// class Book extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <div>
+//           <div>
+//             <select>
+//               <option value="move" disabled>
+//                 Move to...
+//               </option>
+//               <option value="currentlyReading">Currently Reading</option>
+//               <option value="wantToRead">Want to Read</option>
+//               <option value="read">Read</option>
+//               <option value="none">None</option>
+//             </select>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default Book;
+// export default Book;
